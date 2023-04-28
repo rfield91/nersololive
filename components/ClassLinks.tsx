9818007d8170bf1c0b3bea1e4645e74bb515548b
Kyle Murray <kylemurray@Kyles-MacBook-Air.local>
2023-04-28 07:56:33 -0400

    Adding the ability to filter classes, rather than just scrolling to them

--- components/ClassLinks.tsx
+++ components/ClassLinks.tsx
@@ -1,21 +1,55 @@
 import Link from "next/link";
+import React, { Dispatch } from 'react';
 
-const ClassLinks = ({ classes }: { classes: string[] }) => {
+interface ClassLinksProps {
+    classes: string[],
+    filteredClasses: string[],
+    setFilteredClasses: Dispatch<((c: string[]) => string[]) | string[]>
+}
+
+const ClassLinks = ({ classes, filteredClasses, setFilteredClasses }: ClassLinksProps) => {
+
+    const handleFilteredClasses = (name: string) => {
+        setFilteredClasses((current: string[]) => {
+            // remove item if it is already in the array, or add if it is not
+            const index = current.indexOf(name);
+            index === -1 ? current.push(name) : current.splice(index, 1);
+            return [...current];
+        })
+    }
+
+    const noSelected = !filteredClasses.length;
+    const showStyle = 'bg-blue-500 text-white';
+    const hideStyle = 'bg-white text-blue-500 border border-blue-500';
+
     const classLinks = classes.map((c) => {
+        const selected = filteredClasses.includes(c);
         return (
             <Link
                 key={c}
-                href={`#${c}`}
-                className="bg-blue-500 text-white font-bold col-span-2 m-1 text-center rounded-xl"
+                href={`#`}
+                onClick={() => handleFilteredClasses(c)}
+                className={`${selected || noSelected ? showStyle : hideStyle} font-bold col-span-2 m-1 text-center rounded-xl`}
             >
                 {c}
             </Link>
         );
     });
+
     return (
         <div className="bg-white shadow-lg p-2 mt-2">
-            <div className="grid grid-cols-12">{classLinks}</div>
-        </div>
+            <div className="grid grid-cols-12">
+                {classLinks}
+                {!noSelected && <Link
+                    key={`clear`}
+                    href={`#`}
+                    onClick={() => setFilteredClasses([])}
+                    className={`bg-orange-500 text-white font-bold col-span-2 col-start-11 m-1 text-center rounded-xl`}
+                >
+                    Clear
+                </Link>}
+            </div>
+        </div >
     );
 };
 
--- components/ClassResults.tsx
+++ components/ClassResults.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { ClassResultsJson } from "../common/types";
 import IndividualClassResults from "./IndividualClassResults";
 import ClassLinks from "./ClassLinks";
 
 const ClassResults = ({ results }: ClassResultsJson) => {
     const classes = Object.keys(results);
+    // const store: string = window.localStorage?.getItem('filteredClasses') || ''
+    // const storedFilterClasses: string[] = store ? JSON.parse(store) : [];
+    const [filteredClasses, setFilteredClasses] = useState<string[]>([])
 
     const classResults = classes.map((classKey) => {
+        // hide elements that are not selected filters
+        if (filteredClasses.length > 0 && !filteredClasses.includes(classKey)) {
+            return '';
+        }
         return (
             <IndividualClassResults
                 className={classKey}
@@ -17,7 +25,7 @@ const ClassResults = ({ results }: ClassResultsJson) => {
 
     return (
         <div>
-            <ClassLinks classes={classes} />
+            <ClassLinks classes={classes} filteredClasses={filteredClasses} setFilteredClasses={setFilteredClasses} />
             <div>{classResults}</div>
         </div>
     );
