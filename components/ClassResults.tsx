dc21bcf72c7e51ce5a9f1466a5a12c61a5681a07
Mark Field <51247227+rfield91@users.noreply.github.com>
2023-05-05 09:14:20 -0400

    Filterable classes

--- components/ClassResults.tsx
+++ components/ClassResults.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ClassResultsJson } from "../common/types";
 import IndividualClassResults from "./IndividualClassResults";
 import ClassLinks from "./ClassLinks";
@@ -5,7 +6,26 @@ import ClassLinks from "./ClassLinks";
 const ClassResults = ({ results }: ClassResultsJson) => {
     const classes = Object.keys(results);
 
+    const [filteredClasses, setFilteredClasses] = useState<string[]>([]);
+
+    const handleFilteredClasses = (toggleClass: string) => {
+        setFilteredClasses((current: string[]) => {
+            // remove item if it is already in the array, or add if it is not
+            const index = current.indexOf(toggleClass);
+            index === -1 ? current.push(toggleClass) : current.splice(index, 1);
+            return [...current];
+        });
+    };
+
+    const clearFilteredClasses = () => {
+        setFilteredClasses([]);
+    };
+
     const classResults = classes.map((classKey) => {
+        // hide elements that are not selected filters
+        if (filteredClasses.length > 0 && !filteredClasses.includes(classKey)) {
+            return "";
+        }
         return (
             <IndividualClassResults
                 className={classKey}
@@ -17,7 +37,12 @@ const ClassResults = ({ results }: ClassResultsJson) => {
 
     return (
         <div>
-            <ClassLinks classes={classes} />
+            <ClassLinks
+                classes={classes}
+                filteredClasses={filteredClasses}
+                toggleFilter={handleFilteredClasses}
+                clearFilters={clearFilteredClasses}
+            />
             <div>{classResults}</div>
         </div>
     );
