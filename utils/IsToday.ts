0ef60efe4e553eb87212208bbd355791b3db64cc
Mark Field <51247227+rfield91@users.noreply.github.com>
2024-01-15 07:10:54 -0500

    Deleted old version

--- utils/IsToday.ts
+++
@@ -1,9 +0,0 @@
-export default function (date: Date) {
-    const now = new Date();
-
-    return (
-        date.getDate() === now.getDate() &&
-        date.getMonth() === now.getMonth() &&
-        date.getFullYear() === now.getFullYear()
-    );
-}
