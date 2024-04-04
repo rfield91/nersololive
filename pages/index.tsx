8c3e2aee70e0466cdecc1277c5c02062cc5b2267
Mark Field <51247227+rfield91@users.noreply.github.com>
2024-04-03 20:14:58 -0400

    Fixed bug causing site to always show rallycross scoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,80 +2,78 @@ import { ClassResult, ClassResultsJson } from "../common/types";
 import ClassResults from "../components/ClassResults";
 
 export async function getServerSideProps() {
-    const res = await fetch(process.env.CLASS_RESULTS_JSON_URL);
-    const json: ClassResultsJson = await res.json();
-    const results = json.results;
+  const res = await fetch(process.env.CLASS_RESULTS_JSON_URL);
+  const json: ClassResultsJson = await res.json();
+  const results = json.results;
 
-    if (process.env.CLASS_RESULT_DISPLAY_MODE) {
-        const classes = Object.keys(results);
+  if (process.env.CLASS_RESULT_DISPLAY_MODE === "rallycross") {
+    const classes = Object.keys(results);
 
-        classes.forEach((c) => {
-            results[c].forEach((entry) => {
-                const runInfo = entry.runInfo;
+    classes.forEach((c) => {
+      results[c].forEach((entry) => {
+        const runInfo = entry.runInfo;
 
-                // Pad missing runs
-                if (runInfo.runs.length < process.env.EXPECTED_RUNS) {
-                    for (
-                        var i = runInfo.runs.length;
-                        i < process.env.EXPECTED_RUNS;
-                        i++
-                    ) {
-                        runInfo.runs.push({
-                            number: i + 1,
-                            status: "CLEAN",
-                            time: 100,
-                            coneCount: 0,
-                            isBest: false,
-                        });
-                    }
-                }
-
-                runInfo.rallyCrossTime = runInfo.runs.reduce(
-                    (partialTotal, r) =>
-                        partialTotal +
-                        (r.status == "DNF"
-                            ? r.time + 10
-                            : r.time + r.coneCount * 2),
-                    0
-                );
+        // Pad missing runs
+        if (runInfo.runs.length < process.env.EXPECTED_RUNS) {
+          for (
+            var i = runInfo.runs.length;
+            i < process.env.EXPECTED_RUNS;
+            i++
+          ) {
+            runInfo.runs.push({
+              number: i + 1,
+              status: "CLEAN",
+              time: 100,
+              coneCount: 0,
+              isBest: false,
             });
+          }
+        }
 
-            results[c].sort(
-                (a, b) => a.runInfo.rallyCrossTime - b.runInfo.rallyCrossTime
-            );
+        runInfo.rallyCrossTime = runInfo.runs.reduce(
+          (partialTotal, r) =>
+            partialTotal +
+            (r.status == "DNF" ? r.time + 10 : r.time + r.coneCount * 2),
+          0
+        );
+      });
 
-            for (let i = 0; i < results[c].length; i++) {
-                results[c][i].position = (i + 1).toString();
+      results[c].sort(
+        (a, b) => a.runInfo.rallyCrossTime - b.runInfo.rallyCrossTime
+      );
 
-                if (i > 0) {
-                    results[c][i].runInfo.rallyCrossToFirst =
-                        results[c][i].runInfo.rallyCrossTime -
-                        results[c][0].runInfo.rallyCrossTime;
-                    results[c][i].runInfo.rallyCrossToNext =
-                        results[c][i].runInfo.rallyCrossTime -
-                        results[c][i - 1].runInfo.rallyCrossTime;
-                }
-            }
-        });
-    }
+      for (let i = 0; i < results[c].length; i++) {
+        results[c][i].position = (i + 1).toString();
 
-    return {
-        props: {
-            results: results,
-            activePage: "classResults",
-            displayMode: process.env.CLASS_RESULT_DISPLAY_MODE,
-            expectedRuns: process.env.EXPECTED_RUNS,
-        },
-    };
+        if (i > 0) {
+          results[c][i].runInfo.rallyCrossToFirst =
+            results[c][i].runInfo.rallyCrossTime -
+            results[c][0].runInfo.rallyCrossTime;
+          results[c][i].runInfo.rallyCrossToNext =
+            results[c][i].runInfo.rallyCrossTime -
+            results[c][i - 1].runInfo.rallyCrossTime;
+        }
+      }
+    });
+  }
+
+  return {
+    props: {
+      results: results,
+      activePage: "classResults",
+      displayMode: process.env.CLASS_RESULT_DISPLAY_MODE,
+      expectedRuns: process.env.EXPECTED_RUNS,
+    },
+  };
 }
 
 type ClassResultsPageProps = {
-    results: { [name: string]: ClassResult[] };
-    displayMode: "autocross" | "rallycross";
+  results: { [name: string]: ClassResult[] };
+  displayMode: "autocross" | "rallycross";
 };
 
 const Home = ({ results, displayMode: displayMode }: ClassResultsPageProps) => {
-    return <ClassResults results={results} displayMode={displayMode} />;
+  return <ClassResults results={results} displayMode={displayMode} />;
 };
 
 export default Home;
